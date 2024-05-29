5611f534f8548582702cfb09f236fd942f637234
Luc Siecker <60777661+lsiecker@users.noreply.github.com>
2024-05-29 10:48:10 +0200

    #16: Code Cleanup  (#17)
    * #16: Remove unnecessary imports and prints
    
    * #16: Renamed and deleted files
    
    * #16: Fix testing during push
    
    * #16: Enable workflow on all branches

--- java-project/src/main/java/com/java/r2pgdm/Export.java
+++ java-project/src/main/java/com/java/r2pgdm/Export.java
@@ -6,7 +6,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
